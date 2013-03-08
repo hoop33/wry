37b8c26d22cd3214cc21d56d9917a8c77db53ae7
Rob Warner <rwarner@grailbox.com>
2013-03-08 06:28:58 -0500

    Initial Commit

---
+++ wry/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  wry
+//
+//  Created by Rob Warner on 3/8/13.
+//  Copyright (c) 2013 Rob Warner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
