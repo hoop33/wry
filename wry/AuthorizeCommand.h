20d295a48cbde096c1458c1b8156904cafb58a9f
Rob Warner <rwarner@grailbox.com>
2013-03-12 05:01:52 -0400

    Add Authorize command

---
+++ wry/AuthorizeCommand.h
@@ -0,0 +1,14 @@
+//
+//  AuthorizeCommand.h
+//  wry
+//
+//  Created by Rob Warner on 3/12/13.
+//  Copyright (c) 2013 Rob Warner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "WryCommand.h"
+
+@interface AuthorizeCommand : NSObject<WryCommand>
+
+@end
---
+++ wry/AuthorizeCommand.m
@@ -0,0 +1,20 @@
+//
+//  AuthorizeCommand.m
+//  wry
+//
+//  Created by Rob Warner on 3/12/13.
+//  Copyright (c) 2013 Rob Warner. All rights reserved.
+//
+
+#import "AuthorizeCommand.h"
+
+@implementation AuthorizeCommand
+
+- (int)run:(WryApplication *)app params:(NSArray *)params {
+  return 0;
+}
+
+- (void)showHelp {
+}
+
+@end
