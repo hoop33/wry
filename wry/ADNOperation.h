5312f5cdf397895729db557f04d2a9289ef19110
Rob Warner <rwarner@grailbox.com>
2013-03-12 17:41:45 -0400

    Add ADNOperation class to implement the URL call on an NSOperation

---
+++ wry/ADNOperation.h
@@ -0,0 +1,11 @@
+//
+//  ADNOperation.h
+//  wry
+//
+//  Created by Rob Warner on 3/12/13.
+//  Copyright (c) 2013 Rob Warner. All rights reserved.
+//
+
+@interface ADNOperation : NSOperation
+
+@end
---
+++ wry/ADNOperation.m
@@ -0,0 +1,13 @@
+//
+//  ADNOperation.m
+//  wry
+//
+//  Created by Rob Warner on 3/12/13.
+//  Copyright (c) 2013 Rob Warner. All rights reserved.
+//
+
+#import "ADNOperation.h"
+
+@implementation ADNOperation
+
+@end
