ae1412843303cbf4d8310d821c4619499e731639
Rob Warner <rwarner@grailbox.com>
2013-03-21 18:50:22 -0400

    Show help for the command passed to the help command

--- wry/HelpCommand.m
+++ wry/HelpCommand.m
@@ -7,25 +7,38 @@
 //
 
 #import "HelpCommand.h"
+#import "WryErrorCodes.h"
 
 @implementation HelpCommand
 
 - (BOOL)run:(WryApplication *)app params:(NSArray *)params error:(NSError **)error {
+  BOOL success = YES;
   if (params.count == 0) {
     [app println:[NSString stringWithFormat:@"usage: %@ <command> [<params>]", app.appName]];
     [app println:@""];
     [app println:[NSString stringWithFormat:@"The %@ commands are:", app.appName]];
     for (Class cls in [app allCommands]) {
-      id<WryCommand> command = [[cls alloc] init];
+      id <WryCommand> command = [[cls alloc] init];
       // TODO go through app println but with fieldwidth
       printf("%-15s%s\n", [[app nameForCommand:command] UTF8String], [[command summary] UTF8String]);
     }
   }
   else {
-    // Get the command
-    // Show the help
+    id <WryCommand> command = [app commandForName:[params objectAtIndex:0]];
+    if (command == nil) {
+      if (error != NULL) {
+        *error = [NSError errorWithDomain:app.errorDomain
+                                     code:WryErrorCodeBadInput
+                                 userInfo:@{NSLocalizedDescriptionKey : [NSString stringWithFormat:@"%@ is not a valid %@ command",
+                                                                                                   [params objectAtIndex:0],
+                                                                                                   app.appName]}];
+      }
+      success = NO;
+    } else {
+      [app println:[command help]];
+    }
   }
-  return YES;
+  return success;
 }
 
 - (NSString *)help {
