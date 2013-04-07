16f97865932e48157b1e5baaf1a32f3fa1384d20
Rob Warner <rwarner@grailbox.com>
2013-04-07 13:10:09 -0400

    Add Alfred formatter

---
+++ wry/ALFREDFormatter.h
@@ -0,0 +1,13 @@
+//
+//  ALFREDFormatter.h
+//  wry
+//
+//  Created by Rob Warner on 4/7/13.
+//  Copyright (c) 2013 Rob Warner. All rights reserved.
+//
+
+#import "WryFormatter.h"
+
+@interface ALFREDFormatter : NSObject <WryFormatter>
+
+@end
