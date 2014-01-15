6cf015ab0a6fbfb091dd5dde7bb0ab18d5bb14ec
Rob Warner <rwarner@grailbox.com>
2014-01-15 15:36:07 -0500

    Add tests for WrySettings

--- wry Tests/WrySettingsTests.m
+++ wry Tests/WrySettingsTests.m
@@ -9,6 +9,9 @@
 #import <XCTest/XCTest.h>
 #import "WrySettings.h"
 #import "UserSetting.h"
+#import "CountSetting.h"
+#import "DebugSetting.h"
+#import "ColorsSetting.h"
 
 @interface WrySettingsTests : XCTestCase
 
@@ -18,22 +21,33 @@
   WrySettings *settings;
 }
 
-- (void)setUp
-{
+- (void)setUp {
   [super setUp];
   settings = [[WrySettings alloc] init];
 }
 
-- (void)tearDown
-{
+- (void)tearDown {
   settings = nil;
   [super tearDown];
 }
 
-- (void)testSetTransientValueShouldSetValue
-{
+- (void)testSetTransientValueShouldSetStringValue {
   [settings setTransientValue:@"TestUser" forSetting:[[UserSetting alloc] init]];
   XCTAssertEqualObjects([settings stringValue:@"user"], @"TestUser", @"The user setting should be 'TestUser'");
+}
+
+- (void)testSetTransientValueShouldIntegerValue {
+  [settings setTransientValue:@"7" forSetting:[[CountSetting alloc] init]];
+  XCTAssertEqualObjects([NSNumber numberWithInteger:[settings integerValue:@"count"]], @7, @"The count setting should be 7");
+}
+
+- (void)testSetTransientValueShouldSetBoolValue {
+  [settings setTransientValue:@"whatever" forSetting:[[DebugSetting alloc] init]];
+  XCTAssertTrue([settings boolValue:@"debug"], @"The debug setting should be true");
+}
+
+- (void)testDefaultFormatShouldBeText {
+  XCTAssertEqualObjects([settings stringValue:@"format"], @"text", @"The default format should be text");
 }
 
 @end
