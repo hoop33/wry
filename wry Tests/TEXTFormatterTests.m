0ef9270e79e1f0608c2821af007164372d2a3500
Rob Warner <rwarner@grailbox.com>
2014-05-01 10:14:13 -0400

    Remove extra spaces

--- wry Tests/TEXTFormatterTests.m
+++ wry Tests/TEXTFormatterTests.m
@@ -34,14 +34,14 @@
 
 - (void)testPostsOutputShouldShowRange {
   NSData *data = [NSData dataWithContentsOfFile:[self resourcePath:@"posts.json"]];
-  ADNResponse *response = [[ADNResponse alloc] initWithData:data mapping:[ADNMappingProvider  postMapping] reverse:NO error:nil];
+  ADNResponse *response = [[ADNResponse alloc] initWithData:data mapping:[ADNMappingProvider postMapping] reverse:NO error:nil];
   NSString *output = [formatter format:response];
   XCTAssertNotEqual([output rangeOfString:@"2 items (29154298 - 29269343)"].location, NSNotFound, @"The range of posts should be in the output");
 }
 
 - (void)testPostOutputRangeShouldShowSingleItem {
   NSData *data = [NSData dataWithContentsOfFile:[self resourcePath:@"post.json"]];
-  ADNResponse *response = [[ADNResponse alloc] initWithData:data mapping:[ADNMappingProvider  postMapping] reverse:NO error:nil];
+  ADNResponse *response = [[ADNResponse alloc] initWithData:data mapping:[ADNMappingProvider postMapping] reverse:NO error:nil];
   NSString *output = [formatter format:response];
   XCTAssertNotEqual([output rangeOfString:@"1 item (29415319)"].location, NSNotFound, @"The single post should be in the output");
 }
