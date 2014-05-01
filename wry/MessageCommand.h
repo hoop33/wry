7221815c71328894d67ffffec34e0b647026d43b
Rob Warner <rwarner@grailbox.com>
2014-05-01 12:47:16 -0400

    Add message command to retrieve a single message

---
+++ wry/MessageCommand.h
@@ -0,0 +1,14 @@
+//
+//  MessageCommand.h
+//  wry
+//
+//  Created by Rob Warner on 5/1/14.
+//  Copyright (c) 2014 Rob Warner. All rights reserved.
+//
+
+#import "WryCommand.h"
+#import "BaseCommand.h"
+
+@interface MessageCommand : BaseCommand <WryCommand>
+
+@end
