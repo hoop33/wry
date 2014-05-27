e1e1c548372cbe756c422d8971d91622138e2bf8
Rob Warner <rwarner@grailbox.com>
2014-05-27 10:26:07 -0400

    Add an autorelease pool to the shell so memory gets released

--- wry/ShellCommand.m
+++ wry/ShellCommand.m
@@ -108,19 +108,21 @@ void completion(const char *buf, linenoiseCompletions *lc) {
     if ([@[@"quit", @"exit"] containsObject:[input lowercaseString]]) {
       break;
     } else {
-      WryCommandLine *commandLine = [[WryCommandLine alloc] init];
-      if ([commandLine parseString:input error:error]) {
-        // No shells in shell
-        if ([commandLine.commandName isEqualToString:[WryUtils nameForCommand:self]]) {
-          [[WryApplication application] println:@"That would make turtles all the way down."];
+      @autoreleasepool {
+        WryCommandLine *commandLine = [[WryCommandLine alloc] init];
+        if ([commandLine parseString:input error:error]) {
+          // No shells in shell
+          if ([commandLine.commandName isEqualToString:[WryUtils nameForCommand:self]]) {
+            [[WryApplication application] println:@"That would make turtles all the way down."];
+          } else {
+            linenoiseHistoryAdd(line);
+            linenoiseHistorySave(historyFile);
+            [commandLine run];
+          }
         } else {
-          linenoiseHistoryAdd(line);
-          linenoiseHistorySave(historyFile);
-          [commandLine run];
-        }
-      } else {
-        if (error != NULL) {
-          [[WryApplication application] println:[*error localizedDescription]];
+          if (error != NULL) {
+            [[WryApplication application] println:[*error localizedDescription]];
+          }
         }
       }
     }
